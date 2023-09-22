90c08193265729dad7a6d4cc1e8f887cd36dedb2
Souza <114781129+Souzinhaaz@users.noreply.github.com>
2023-09-22 11:31:01 -0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -21,13 +21,48 @@ def menu():
 def cadastrar():
     # Limpa o terminal
     os.system("cls")
-    
-    print(f"{'-' * 15} Cadastro {'-' * 15}")
-    nome_turma = input("Digite o nome da turma: ")
-    
-    if not os.path.exists("turmas.json"): 
-        with open("turmas.json", "w") as turmas:
-            json.dump(nome_turma, "turmas.json")
-    
+
+    opcao = input(f""""{'-' * 15} Cadastro {'-' * 15}
+
+1 - Cadastrar Turma
+2 - Cadastrar Aluno
+3 - Cadastrar Boletim
+
+""")
+
+    if opcao == "1":
+        if not os.path.exists("turmas.json"):
+            with open("turmas.json", "w") as turmas:
+                id = 1
+                while True:
+                    nome_turma = input("Digite o nome da turma: ")
+                    turno_turma = input("Digite o turno da sua turma: ")
+                    ano_turma = input("Digite o ano da turma: ")
+
+                    # Adiciona os dados no turmas.json
+                    turmas[id] = {"Nome": nome_turma, "Turno": turno_turma, "Ano": ano_turma}
+                    perg = input("Você quer cadastrar outra turma? Sim ou Não? ").upper()
+
+                    if perg[0] == "S":
+                        id += 1
+                    else:
+                        break
+
+        else:
+            id = 2
+            while True:
+                nome_turma = input("Digite o nome da turma: ")
+                turno_turma = input("Digite o turno da sua turma: ")
+                ano_turma = input("Digite o ano da turma: ")
+
+                # Adiciona os dados no turmas.json
+                turmas[id] = {"Nome": nome_turma, "Turno": turno_turma, "Ano": ano_turma}
+                perg = input("Você quer cadastrar outra turma? Sim ou Não? ").upper()
+
+                if perg[0] == "S":
+                    id += 1
+                else:
+                    break
+
 
 
