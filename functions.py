bee41a301973e0b373497a4f1b31270e15d4e220
Souza <114781129+Souzinhaaz@users.noreply.github.com>
2023-09-25 10:58:54 -0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -36,14 +36,13 @@ def cadastrar(parametro):
     verifica()
 
     # Abre os arquivos a serem usados
-    with open("turmas.json", 'r+', encoding="utf-8") as turmas_json,  open("alunos.json", "r+", encoding="utf-8") as alunos_json, open("boletim.json", 'r+', encoding="utf-8") as boletins_json:
+    with open("turmas.json", 'r+', encoding="utf-8") as turmas_json, open("alunos.json", "r+", encoding="utf-8") as alunos_json, open("boletim.json", 'r+', encoding="utf-8") as boletins_json:
         # Usuário escolhe a opção para cadastrar turma
         if parametro == "1":
             # a variavel nova_turma vai receber o dicionário que está no turmas_json
             nova_turma = json.load(turmas_json)
 
             print(f"{'-' * 15} Cadastro Turmas {'-' * 15}\n")
-
 
             # Vai pedir as informações do usuário e as verificar
             nome_turma = input("Digite o nome da turma: ")
@@ -99,8 +98,10 @@ def cadastrar(parametro):
             os.system("cls")
                 
         elif parametro == "2":
-            # A variavel novo_aluno vai receber o dicionário que está no alunos_json
+            # A variavel novo_aluno vai receber o dicionário que está no alunos_json e a nova_turma o que está em turmas_json
             novo_aluno = json.load(alunos_json)
+            nova_turma = json.load(turmas_json)
+
             print(f"{'-' * 15} Cadastro Alunos {'-' * 15}\n")
 
             # Vai pedir as informações do aluno
@@ -108,8 +109,9 @@ def cadastrar(parametro):
             email_aluno = input("Digite o email do(a) aluno(a): ")
             telefone_aluno = input("Digite o telefone do(a) responsável do aluno(a): ")
             turma = input("Insira o código da turma: ")
+
             # Verifica se a turma existe no arquivo json
-            if turma not in json.load(turmas_json):
+            if turma not in nova_turma:
                 print("Turma inserida não existe no sistema, por favor insira outra!!")
                 turma = input("Insira o código da turma: ") 
 
@@ -117,7 +119,7 @@ def cadastrar(parametro):
 
             # Adiciona as informações do aluno no dicionário 
             novo_aluno[matricula] = {
-                "ID Turma": turmas_json[turma]["Nome"],
+                "Turma": nova_turma[turma]["Nome"],
                 "Nome": nome_aluno,
                 "Email": email_aluno,
                 "Telefone": telefone_aluno,
@@ -137,12 +139,55 @@ def cadastrar(parametro):
             os.system("cls")
 
         elif parametro == "3":
-            # A variavel novo_aluno vai receber o dicionário que está no alunos_json
+            # A variavel novo_boletim vai receber o dicionário que está no boletins_json e o novo_aluno o que está em alunos_json
             novo_boletim = json.load(boletins_json)
+            novo_aluno = json.load(alunos_json)
+
             print(f"{'-' * 15} Cadastro Alunos {'-' * 15}\n")
 
             # Vai pedir o id do aluno referido
             id_aluno = input("Digite o numero de matricula do Aluno referido: ")
+
+            # Vai guardar a quantidade de faltas na variável qnt_faltas
+            qnt_faltas = int(input("Digite a quantidade de faltas do aluno inserido: "))
+
+            # Vai guardar as notas do aluno em uma lista
+            n1 = float(input("Digite a primeira nota do aluno inserido: "))
+            n2 = float(input("Digite a segunda nota do aluno inserido: "))
+            n3 = float(input("Digite a terceira nota do aluno inserido: "))
+            n4 = float(input("Digite a quarta nota do aluno inserido: "))
+            notas_aluno = [n1, n2, n3, n4]
+            media = sum(notas_aluno) / len(notas_aluno)
+            aprovado = True
+            situacao = ""
+
+            # Faz a verificação para saber se o aluno está aprovado ou não
+            if media >= 7 and qnt_faltas < 15:
+                aprovado = True
+            else:
+                aprovado = False
+                
+            if aprovado:
+                situacao = "Aprovado"
+            else:
+                situacao = "Reprovado"
+
+            # Faz a verificação para saber se o id do aluno existe no alunos_json
+            if id_aluno not in novo_aluno:
+                print("Aluno inserido não existe, por favor digite um existente.")
+                id_aluno = input("Digite o número de matricula do Aluno referido: ")
+
+            # Calculo para obter o novo id do boletim
+            id_boletim = str(1+int(list(novo_boletim.keys())[-1])) if len(novo_boletim) > 0 else "1"
+            
+            novo_boletim[id_boletim] = {
+                "Matrícula": id_aluno,
+                "Nome": novo_aluno[id_aluno]["Nome"],
+                "Turma": novo_aluno[id_aluno]["Turma"],
+                "Notas": notas_aluno,
+                "Quantidade de Faltas": qnt_faltas,
+                "Situação": situacao
+            } 
 
 
 
